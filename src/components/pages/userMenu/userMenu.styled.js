37838bed0ac4afa164cd447cbaa7c6ca3a2c0ac7
Ihor <132585512+IhorZolot@users.noreply.github.com>
2023-08-23 09:28:56 +0300

    finaly 3

--- src/components/pages/userMenu/userMenu.styled.js
+++ src/components/pages/userMenu/userMenu.styled.js
@@ -1,35 +1,49 @@
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const UserMenuContainerStyled = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	font-size: 16px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 16px;
+  margin-top: 100px;
+`;
 
 export const UserInfoStyled = styled.p`
-	font-weight: bold;
-`
+  font-weight: bold;
+`;
 
 export const LogoutButtonStyled = styled.button`
-	background-color: #ff006f;
-	color: white;
-	border: none;
-	padding: 5px 10px;
-	border-radius: 5px;
-	cursor: pointer;
-`
+  background-color: #ff006f;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    color: #160909;
+    background-color: #ffffff;
+  }
+`;
 export const InlineParagraph = styled.p`
-	display: inline;
-	margin: 0;
-	padding: 0;
-	font-size: 16px;
-	color: #333;
-`
+  display: inline;
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  color: #333;
+`;
 export const StyledLink = styled(Link)`
-	color: #5175d0;
-	text-decoration: underline;
-	margin-top: 10px;
-	display: block;
-`
+  margin-top: 20px;
+  display: block;
+  font-size: 20px;
+  text-decoration: none;
+  border: 1px solid #160909;
+  border-radius: 5px;
+  padding: 5px 10px;
+  color: #5175d0;
+  font-weight: bold;
+  &:hover {
+    color: #160909;
+    background-color: #5175d0;
+  }
+`;
