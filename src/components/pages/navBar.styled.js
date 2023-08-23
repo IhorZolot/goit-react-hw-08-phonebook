14119ca8eca11d46d001675b862ad0078a0b11d8
Ihor <132585512+IhorZolot@users.noreply.github.com>
2023-08-23 09:14:02 +0300

    finaly

--- src/components/pages/navBar.styled.js
+++ src/components/pages/navBar.styled.js
@@ -1,46 +1,45 @@
-import { NavLink } from 'react-router-dom'
-import styled from 'styled-components'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const SideBarStyled = styled.nav`
-	background-color: #5175d0;
-	box-shadow: 0px 2px 4px rgba(0, 0, 0.1, 0.5);
-	display: flex;
-	gap: 25px;
-	top: 0;
-	left: 0;
-	width: 100%;
-	padding: 20px;
-	justify-content: space-between;
-	margin-bottom: 100px;
-`
+  background-color: #5175d0;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0.1, 0.5);
+  display: flex;
+  gap: 25px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 20px;
+  justify-content: space-between;
+`;
 
 export const ButtonContainerStyled = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 10px;
-	margin-right: 60px;
-`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-right: 60px;
+`;
 
 export const NavLinkButton = styled(NavLink)`
-	text-decoration: none;
-	padding: 5px 10px;
-	border: 1px solid #160909;
-	border-radius: 5px;
-	color: #160909;
-	font-weight: bold;
-	&:hover {
-		background-color: #160909;
-		color: #ffffff;
-	}
-`
+  text-decoration: none;
+  padding: 5px 10px;
+  border: 1px solid #160909;
+  border-radius: 5px;
+  color: #160909;
+  font-weight: bold;
+  &:hover {
+    background-color: #160909;
+    color: #ffffff;
+  }
+`;
 
 export const StyledLink = styled(NavLink)`
-	margin-left: 40px;
-	font-size: 20px;
-	text-decoration: none;
-	color: #ffffff;
-	font-weight: bold;
-	&:hover {
-		color: #160909;
-	}
-`
+  margin-left: 40px;
+  font-size: 20px;
+  text-decoration: none;
+  color: #ffffff;
+  font-weight: bold;
+  &:hover {
+    color: #160909;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Slide, ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import './index.css'
-import { Provider } from 'react-redux'
-import { store } from 'redux/store'
-import { BrowserRouter } from 'react-router-dom'
-import { App } from 'components/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Slide, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<>
-		<BrowserRouter basename='goit-react-hw-08-phonebook'>
-			<Provider store={store}>
-				<App />
-				<ToastContainer autoClose={1500} hideProgressBar transition={Slide} />
-			</Provider>
-		</BrowserRouter>
-	</>
-)
+  <>
+    <BrowserRouter basename="goit-react-hw-08-phonebook">
+      <Provider store={store}>
+        <App />
+        <ToastContainer autoClose={1500} hideProgressBar transition={Slide} />
+      </Provider>
+    </BrowserRouter>
+  </>
+);
